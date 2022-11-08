9b8ca1cbc6e90dd742a0a77a6b2965ea3d8cac1a
Satorrid <Devintsefton@gmail.com>
2022-11-08 10:41:22 -0800

    fleshed out contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,66 @@
+import { useState } from "react";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleNameBlur() {
+    if (name === "") {
+      alert("Name is required");
+    }
+  }
+
+  function handleEmailBlur() {
+    if (email === "") {
+      alert("Email is required");
+      return;
+    }
+    if (!/^.+\@.+\..+$/.test(email)) {
+      alert("Email must be valid");
+    }
+  }
+
+  function handleMessageBlur() {
+    if (message === "") {
+      alert("A message is required");
+    }
+  }
+
+  return (
+    <div>
+      <h1>Contact</h1>
+      <form>
+        <label>
+          Name:
+          <input
+            required
+            onBlur={handleNameBlur}
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            required
+            type="email"
+            onBlur={handleEmailBlur}
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          Message:
+          <textarea
+            required
+            onBlur={handleMessageBlur}
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
